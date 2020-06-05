38a208d4c2351ae1fd0149bfbb423615f9751aed
Grisel Matos <griselmatosm@gmail.com>
2020-06-05 14:49:27 +0200

    Actual Code Reusegit add -A! Create useReducers function to separate all the logic around fetching

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
   const fetchResources = async (resource) => {
@@ -12,6 +12,12 @@ const ResourceList = ({ resource }) => {
   useEffect(() => {
     fetchResources(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
